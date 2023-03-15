a556dd5cf5d772132bd22affc722e4c1bd1143a1
abhishektiwari-knoldus <126809890+abhishektiwari-knoldus@users.noreply.github.com>
2023-03-15 17:39:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node i am abhishek tiwari from prayag !\n'
+  const msg = 'Hello Node i am abhishek tiwari from prayagraj !\n'
   res.end(msg);
 });
 
